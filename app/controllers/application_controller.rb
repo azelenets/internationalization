12cd3f92f6e464e238df6354e635ea79f9b205c3
Andrew Zelenets <andrew.zelenets@gmail.com>
2017-09-05 15:20:30 +0300

    Add I18n initializer and add setting locale from params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale_from_params
+
+  private
+
+  def set_locale_from_params
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
